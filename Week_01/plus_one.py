6bf54f8c086ddc4a199b152c28e25ae4697d5828
Orcs <jay1412008@163.com>
2020-09-10 00:07:43 +0800

    使用递归解决加一问题

--- Week_01/plus_one.py
+++ Week_01/plus_one.py
@@ -29,3 +29,19 @@ class SolutionO:
             return [1] + newlst
         digits[-1] += 1
         return digits + newlst
+
+    def plusOne2(self, digits: List[int]) -> List[int]:
+        """
+        使用递归处理
+        :param digits:
+        :return:
+        """
+        if digits[-1] < 9:
+            digits[-1] += 1
+            return digits
+        elif len(digits) == 1 and digits[0] == 9:
+            return [1, 0]
+        else:
+            digits[-1] = 0
+            digits[0:-1] = self.plusOne(digits[0:-1])
+            return digits
