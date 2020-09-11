c5fb7fecf72a538b87152afc510847ccd2ee0b7d
Orcs <jay1412008@163.com>
2020-09-12 00:33:47 +0800

    Update move_zeroes.py

--- Week_01/move_zeroes.py
+++ Week_01/move_zeroes.py
@@ -2,6 +2,7 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        从数组头部开始循环,判断值是否等于0，如果是则弹出该值，并且在list末尾追加0，否则什么都不做，
         """
         pre_idx = 0
         n = len(nums)
