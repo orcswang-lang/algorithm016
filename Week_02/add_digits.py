eb27296a8c96c1f931b7c4d72485b07846817354
Orcs <jay1412008@163.com>
2020-09-18 23:30:33 +0800

    258. 各位相加

---
+++ Week_02/add_digits.py
@@ -0,0 +1,26 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num > 9:
+            count =0
+            while num >0:
+                count += num % 10
+                num //=10
+            num = count
+        return num
+
+    def addDigits1(self, num: int) -> int:
+        if num < 9: return num
+        n = num % 9
+        if n == 0: return 9
+        return n
+
+    def addDigits2(self, num: int) -> int:
+        while num > 9:
+            num = sum([int(digit) for digit in str(num)])
+        return num
+
+    def addDigits3(self, num: int) -> int:
+        return (num - 1) % 9 + 1 if num != 0 else 0
+
+    def addDigits4(self, num: int) -> int:
+        return num % 9 or 9 * bool(num)
