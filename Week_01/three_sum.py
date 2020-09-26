5a97985c8371e6c046bf7b4e051011c99354ea84
Orcs <jay1412008@163.com>
2020-09-26 22:36:34 +0800

    三数之和

--- Week_01/three_sum.py
+++ Week_01/three_sum.py
@@ -21,7 +21,58 @@
 
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
+
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        nums.sort()
+        ans = list()
+
+        # 枚举 a
+        for first in range(n):
+            # 需要和上一次枚举的数不相同
+            if first > 0 and nums[first] == nums[first - 1]:
+                continue
+            # c 对应的指针初始指向数组的最右端
+            third = n - 1
+            target = -nums[first]
+            # 枚举 b
+            for second in range(first + 1, n):
+                # 需要和上一次枚举的数不相同
+                if second > first + 1 and nums[second] == nums[second - 1]:
+                    continue
+                # 需要保证 b 的指针在 c 的指针的左侧
+                while second < third and nums[second] + nums[third] > target:
+                    third -= 1
+                # 如果指针重合，随着 b 后续的增加
+                # 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if second == third:
+                    break
+                if nums[second] + nums[third] == target:
+                    ans.append([nums[first], nums[second], nums[third]])
+
+        return ans
+
+    def threeSum1(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        N, result = len(nums), []
+        for i in range(N):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            target = nums[i] * -1
+            s, e = i + 1, N - 1
+            while s < e:
+                if nums[s] + nums[e] == target:
+                    result.append([nums[i], nums[s], nums[e]])
+                    s = s + 1
+                    while s < e and nums[s] == nums[s - 1]:
+                        s = s + 1
+                elif nums[s] + nums[e] < target:
+                    s = s + 1
+                else:
+                    e = e - 1
+        return result
+
+    def threeSum2(self, nums: List[int]) -> List[List[int]]:
         if not nums: return []
 
         # 先排序，关键！
