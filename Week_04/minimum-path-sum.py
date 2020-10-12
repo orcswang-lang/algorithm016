f1a53c2e0e64b942b6f754bd7020bb232668e21c
Orcs <jay1412008@163.com>
2020-10-12 22:49:58 +0800

    64. 最小路径和

---
+++ Week_04/minimum-path-sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        rows, columns = len(grid), len(grid[0])
+        dp = [[0] * columns for _ in range(rows)]
+        dp[0][0] = grid[0][0]
+        for i in range(1, rows):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+        for j in range(1, columns):
+            dp[0][j] = dp[0][j - 1] + grid[0][j]
+        for i in range(1, rows):
+            for j in range(1, columns):
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+
+        return dp[rows - 1][columns - 1]
+
+    def minPathSum1(self, grid: List[List[int]]) -> int:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i == j == 0:
+                    continue
+                elif i == 0:
+                    grid[i][j] = grid[i][j - 1] + grid[i][j]
+                elif j == 0:
+                    grid[i][j] = grid[i - 1][j] + grid[i][j]
+                else:
+                    grid[i][j] = min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j]
+        return grid[-1][-1]
