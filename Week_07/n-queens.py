62388a697e45ee4c9c57c1fad20231ae21497a56
Orcs <jay1412008@163.com>
2020-11-01 22:51:31 +0800

    37. 解数独

---
+++ Week_07/n-queens.py
@@ -0,0 +1,63 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        def generateBoard():
+            board = list()
+            for i in range(n):
+                row[queens[i]] = "Q"
+                board.append("".join(row))
+                row[queens[i]] = "."
+            return board
+
+        def backtrack(row: int):
+            if row == n:
+                board = generateBoard()
+                solutions.append(board)
+            else:
+                for i in range(n):
+                    if i in columns or row - i in diagonal1 or row + i in diagonal2:
+                        continue
+                    queens[row] = i
+                    columns.add(i)
+                    diagonal1.add(row - i)
+                    diagonal2.add(row + i)
+                    backtrack(row + 1)
+                    columns.remove(i)
+                    diagonal1.remove(row - i)
+                    diagonal2.remove(row + i)
+
+        solutions = list()
+        queens = [-1] * n
+        columns = set()
+        diagonal1 = set()
+        diagonal2 = set()
+        row = ["."] * n
+        backtrack(0)
+        return solutions
+
+    def solveNQueens1(self, n: int) -> List[List[str]]:
+        def generateBoard():
+            board = list()
+            for i in range(n):
+                row[queens[i]] = "Q"
+                board.append("".join(row))
+                row[queens[i]] = "."
+            return board
+
+        def solve(row: int, columns: int, diagonals1: int, diagonals2: int):
+            if row == n:
+                board = generateBoard()
+                solutions.append(board)
+            else:
+                availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2))
+                while availablePositions:
+                    position = availablePositions & (-availablePositions)
+                    availablePositions = availablePositions & (availablePositions - 1)
+                    column = bin(position - 1).count("1")
+                    queens[row] = column
+                    solve(row + 1, columns | position, (diagonals1 | position) << 1, (diagonals2 | position) >> 1)
+
+        solutions = list()
+        queens = [-1] * n
+        row = ["."] * n
+        solve(0, 0, 0, 0)
+        return solutions
