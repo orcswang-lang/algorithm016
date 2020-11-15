8a96f832da58d0afd74b5703606d922a46f513c3
Orcs <jay1412008@163.com>
2020-11-15 22:45:28 +0800

    5. 最长回文子串

--- Week_09/README.md
+++ Week_09/README.md
@@ -1,1 +1,48 @@
 学习笔记
+### Atoi 代码示例
+```python
+class Solution(object):
+    def myAtoi(self, s):
+        if len(s) == 0 : return 0
+        ls = list(s.strip())
+        
+        sign = -1 if ls[0] == '-' else 1
+        if ls[0] in ['-','+'] : del ls[0]
+        ret, i = 0, 0
+        while i < len(ls) and ls[i].isdigit() :
+            ret = ret*10 + ord(ls[i]) - ord('0')
+            i += 1
+        return max(-2**31, min(sign * ret,2**31-1))
+```
+
+### Rabin-Karp 代码示例
+```python
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        d = 256
+        q = 9997
+        n = len(haystack)
+        m = len(needle)
+        h = pow(d,m-1)%q
+        p = 0
+        t = 0
+        if m > n:
+            return -1
+        for i in range(m): # preprocessing
+            p = (d*p+ord(needle[i]))%q
+            t = (d*t+ord(haystack[i]))%q
+        for s in range(n-m+1): # note the +1
+            if p == t: # check character by character
+                match = True
+                for i in range(m):
+                    if needle[i] != haystack[s+i]:
+                        match = False
+                        break
+                if match:
+                    return s
+            if s < n-m:
+                t = (t-h*ord(haystack[s]))%q
+                t = (t*d+ord(haystack[s+m]))%q
+                t = (t+q)%q
+        return -1
+```
---
+++ Week_09/longest-palindromic-substring.py
@@ -0,0 +1,114 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        if size < 2:
+            return s
+
+        dp = [[False for _ in range(size)] for _ in range(size)]
+
+        max_len = 1
+        start = 0
+
+        for j in range(1, size):
+            for i in range(0, j):
+
+                dp[i][j] = (s[i] == s[j]) and (j - i < 3 or dp[i + 1][j - 1])
+
+                if dp[i][j]:
+                    cur_len = j - i + 1
+                    if cur_len > max_len:
+                        max_len = cur_len
+                        start = i
+        return s[start:start + max_len]
+
+class Solution1:
+    def longestPalindrome(self, s: str) -> str:
+        size = len(s)
+        if size < 2:
+            return s
+
+        # 至少是 1
+        max_len = 1
+        res = s[0]
+
+        for i in range(size):
+            palindrome_odd, odd_len = self.__center_spread(s, size, i, i)
+            palindrome_even, even_len = self.__center_spread(s, size, i, i + 1)
+
+            # 当前找到的最长回文子串
+            cur_max_sub = palindrome_odd if odd_len >= even_len else palindrome_even
+            if len(cur_max_sub) > max_len:
+                max_len = len(cur_max_sub)
+                res = cur_max_sub
+
+        return res
+
+    def __center_spread(self, s, size, left, right):
+        """
+        left = right 的时候，此时回文中心是一个字符，回文串的长度是奇数
+        right = left + 1 的时候，此时回文中心是一个空隙，回文串的长度是偶数
+        """
+        i = left
+        j = right
+
+        while i >= 0 and j < size and s[i] == s[j]:
+            i -= 1
+            j += 1
+        return s[i + 1:j], j - i - 1
+
+#Manacher 算法ß
+class Solution2:
+    def longestPalindrome(self, s: str) -> str:
+        # 特判
+        size = len(s)
+        if size < 2: return s
+
+        # 得到预处理字符串
+        t = "#"
+        for i in range(size):
+            t += s[i]
+            t += "#"
+        # 新字符串的长度
+        t_len = 2 * size + 1
+
+        # 数组 p 记录了扫描过的回文子串的信息
+        p = [0 for _ in range(t_len)]
+
+        # 双指针，它们是一一对应的，须同时更新
+        max_right = 0
+        center = 0
+
+        # 当前遍历的中心最大扩散步数，其值等于原始字符串的最长回文子串的长度
+        max_len = 1
+        # 原始字符串的最长回文子串的起始位置，与 max_len 必须同时更新
+        start = 1
+
+        for i in range(t_len):
+            if i < max_right:
+                mirror = 2 * center - i
+                # 这一行代码是 Manacher 算法的关键所在，要结合图形来理解
+                p[i] = min(max_right - i, p[mirror])
+
+            # 下一次尝试扩散的左右起点，能扩散的步数直接加到 p[i] 中
+            left = i - (1 + p[i])
+            right = i + (1 + p[i])
+
+            # left >= 0 and right < t_len 保证不越界
+            # t[left] == t[right] 表示可以扩散 1 次
+            while left >= 0 and right < t_len and t[left] == t[right]:
+                p[i] += 1
+                left -= 1
+                right += 1
+
+            # 根据 max_right 的定义，它是遍历过的 i 的 i + p[i] 的最大者
+            # 如果 max_right 的值越大，进入上面 i < max_right 的判断的可能性就越大，这样就可以重复利用之前判断过的回文信息了
+            if i + p[i] > max_right:
+                # max_right 和 center 需要同时更新
+                max_right = i + p[i]
+                center = i
+
+            if p[i] > max_len:
+                # 记录最长回文子串的长度和相应它在原始字符串中的起点
+                max_len = p[i]
+                start = (i - max_len) // 2
+        return s[start: start + max_len]
